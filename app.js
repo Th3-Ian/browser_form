23f06dd542677676196609af71b7c81d6ab2d329
Ian Hart <ianhart13@gmail.com>
2021-11-28 19:34:09 -0500

    created basic account form with styling

---
+++ app.js
@@ -0,0 +1,8 @@
+// Form Validation
+
+const email = document.getElementById('emailInput');
+const country = document.getElementById('countryInput');
+const zipcode = document.getElementById('zipInput');
+const password = document.getElementById('passwordInput');
+const passwordConfirm = document.getElementById('passwordConfirmInput');
+
